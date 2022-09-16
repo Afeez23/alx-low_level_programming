61b948c54409a63918c9c7ac86fc81a90cf9d14f
Afeez23 <afeezabiodun23@gmail.com>
2022-09-16 07:29:17 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 	char c;
+
 	c = 'A';
 	printf("%c: %d\n", c, _isupper(c));
-        c = 'a';
+	c = 'a';
 	printf("%c: %d\n", c, _isupper(c));
-        return (0);
+	return (0);
 }
