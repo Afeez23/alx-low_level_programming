4cdf395cb6ec9160bcc51e6b64a6aaaa063793b2
Afeez23 <afeezabiodun23@gmail.com>
2022-09-24 02:55:51 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,33 @@
 #include "main.h"
 /**
- *rot13 - encodes a string using rot13
- *@c: string to convert
- *Return: encoded string
+ *rot13 - encodes strings using rot13.
+ *@s: pointer to string.
+ *
+ *Return: pointer to encoded string.
  */
-char *rot13(char *c)
+char *rot13(char *s)
 {
-	int i, j;
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int stringCount, rotation;
+	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+	'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-	for (i = 0; c[i] != '\0'; i++)
+	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+	'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
 	{
-	for (j = 0; j < 52; j++)
+	for (rotation = 0; rotation < 53; rotation++)
 	{
-	if (c[i] == a[j])
+	if (r1[rotation] == s[stringCount])
 	{
-	c[i] = r[j];
+	s[stringCount] = r2[rotation];
 	break;
 	}
 	}
 	}
-	return (c);
+	return (s);
 }
