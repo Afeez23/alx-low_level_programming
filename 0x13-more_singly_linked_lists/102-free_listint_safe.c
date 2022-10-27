009ac2a5a1129df1c48cf547a24ea49efc4bd468
Afeez23 <afeezabiodun23@gmail.com>
2022-10-27 00:35:44 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -46,7 +46,6 @@ size_t looped_listint_count(listint_t *head)
  * free_listint_safe - Frees a listint_t list safely (ie.
  *                     can free lists containing loops)
  * @h: A pointer to the address of
- *
  *     the head of the listint_t list.
  *
  * Return: The size of the list that was freed.
@@ -59,6 +58,7 @@ size_t free_listint_safe(listint_t **h)
 	size_t nodes, index;
 
 	nodes = looped_listint_count(*h);
+
 	if (nodes == 0)
 	{
 	for (; h != NULL && *h != NULL; nodes++)
